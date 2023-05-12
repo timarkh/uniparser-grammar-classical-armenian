11d406942c3adf1c450a031d58d3450152d17792
Timofey Arkhangelskiy <timarkh@gmail.com>
2023-05-12 17:58:38 +0200

    Fix several issues, add paradigm statistics

--- pre_build.py
+++ pre_build.py
@@ -53,6 +53,61 @@ def prepare_files():
     # shutil.copy2('armenian_disambiguation.cg3', 'uniparser_eastern_armenian/data/')
 
 
+def process_schwa(a):
+    """
+    Find unanalyzed words that contain a schwa inside and try analyzing them
+    without the schwa. Add the results to the list of analyzed words.
+    """
+    rxSchwa = re.compile('^(\\w+)ը(\\w+)$')
+    unanalyzedSchwa = []
+    norm2schwa = {}
+    freqDict = {}
+    with open('wordlists/wordlist_unanalyzed.txt', 'r', encoding='utf-8') as fIn:
+        for word in fIn:
+            word = word.strip()
+            if rxSchwa.search(word) is not None:
+                unanalyzedSchwa.append(word)
+    with open('wordlists/wordlist.csv', 'r', encoding='utf-8') as fIn:
+        for line in fIn:
+            word, freq = line.strip().split('\t')
+            freqDict[word] = freq
+    with open('wordlists/wordlist_schwa.csv', 'w', encoding='utf-8') as fOut:
+        for word in unanalyzedSchwa:
+            wordNorm = rxSchwa.sub('\\1\\2', word)
+            if wordNorm not in norm2schwa:
+                norm2schwa[wordNorm] = [word]
+                fOut.write(wordNorm
+                           + '\t' + freqDict[word] + '\n')
+            else:
+                norm2schwa[wordNorm].append(word)
+    print('Processing schwa words...')
+    a.analyze_wordlist(freqListFile='wordlists/wordlist_schwa.csv',
+                       parsedFile='wordlists/wordlist_analyzed_schwa.txt',
+                       unparsedFile='wordlists/wordlist_unanalyzed_schwa.txt',
+                       verbose=True)
+    analyzedSchwa = set()
+    with open('wordlists/wordlist_analyzed_schwa.txt', 'r', encoding='utf-8') as fIn:
+        lines = '\n'
+        for line in fIn:
+            m = re.search('^(.*>)([^<>\r\n]+)</w>', line)
+            if m is None:
+                continue
+            wordNorm = m.group(2)
+            for word in norm2schwa[wordNorm]:
+                analyzedSchwa.add(word)
+                lines += m.group(1) + word + '</w>\n'
+    with open('wordlists/wordlist_analyzed.txt', 'a', encoding='utf-8') as fOut:
+        fOut.write(lines)
+    lines = []
+    with open('wordlists/wordlist_unanalyzed.txt', 'r', encoding='utf-8') as fIn:
+        for line in fIn:
+            line = line.strip()
+            if line not in analyzedSchwa:
+                lines.append(line)
+    with open('wordlists/wordlist_unanalyzed.txt', 'w', encoding='utf-8') as fOut:
+        fOut.write('\n'.join(lines))
+
+
 def parse_wordlists():
     """
     Analyze wordlists/wordlist.csv.
@@ -69,6 +124,7 @@ def parse_wordlists():
                        parsedFile='wordlists/wordlist_analyzed.txt',
                        unparsedFile='wordlists/wordlist_unanalyzed.txt',
                        verbose=True)
+    process_schwa(a)
 
 
 if __name__ == '__main__':
--- wordlists/freq_unanalyzed.py
+++ wordlists/freq_unanalyzed.py
@@ -1,13 +1,16 @@
+import re
+
+
 freqDict = {}
-with open('wordlist.csv', 'r', encoding='utf-8') as fIn:
+with open('wordlist.csv', 'r', encoding='utf-8-sig') as fIn:
     for line in fIn:
         if '\t' not in line:
             continue
         w, freq = line.strip('\r\n').split('\t')
-        freqDict[w] = freq
+        freqDict[w] = int(freq)
 
 unanalyzedWords = []
-with open('wordlist_unanalyzed.txt', 'r', encoding='utf-8') as fIn:
+with open('wordlist_unanalyzed.txt', 'r', encoding='utf-8-sig') as fIn:
     for line in fIn:
         line = line.strip()
         if len(line) <= 0:
@@ -16,4 +19,62 @@ with open('wordlist_unanalyzed.txt', 'r', encoding='utf-8') as fIn:
 
 with open('wordlist_unanalyzed_freq.txt', 'w', encoding='utf-8') as fOut:
     for w in unanalyzedWords:
-        fOut.write(w + '\t' + freqDict[w] + '\n')
+        fOut.write(w + '\t' + str(freqDict[w]) + '\n')
+
+
+# Collect paradigm statistics
+lemmaFreq = {}
+paradigmFreqToken = {}
+paradigmFreqTypeDict = {}
+paradigmFreqTypeCorp = {}
+with open('wordlist_analyzed.txt', 'r', encoding='utf-8') as fIn:
+    lines = '\n'
+    for line in fIn:
+        m = re.search('^.*>([^<>\r\n]+)</w>', line)
+        if m is None:
+            continue
+        word = m.group(1)
+        freq = freqDict[word]
+        lemmata = re.findall('lex="([^\r\n"<>]+)" gr="[NA]\\b', line)
+        for lemma in lemmata:
+            try:
+                lemmaFreq[lemma] += freq
+            except KeyError:
+                lemmaFreq[lemma] = freq
+
+with open('../xcl-lexemes.txt', 'r', encoding='utf-8') as fIn:
+    text = fIn.read()
+    lexemes = re.findall('-lexeme\n lex: +([^\r\n]+)\n'
+                         ' stem: [^\r\n]*\n'
+                         ' gramm: [NA]\\b[^\r\n]*\n'
+                         ' paradigm: ([^\r\n]+)\n',
+                         text, flags=re.DOTALL)
+    for lemma, paradigm in lexemes:
+        try:
+            paradigmFreqTypeDict[paradigm] += 1
+        except KeyError:
+            paradigmFreqTypeDict[paradigm] = 1
+        if lemma in lemmaFreq:
+            try:
+                paradigmFreqTypeCorp[paradigm] += 1
+            except KeyError:
+                paradigmFreqTypeCorp[paradigm] = 1
+            try:
+                paradigmFreqToken[paradigm] += lemmaFreq[lemma]
+            except KeyError:
+                paradigmFreqToken[paradigm] = lemmaFreq[lemma]
+
+with open('paradigm_type_dict_freq.txt', 'w', encoding='utf-8') as fOut:
+    for paradigm in sorted(paradigmFreqTypeDict,
+                        key=lambda p: (-paradigmFreqTypeDict[p], p)):
+        fOut.write(paradigm + '\t' + str(paradigmFreqTypeDict[paradigm]) + '\n')
+
+with open('paradigm_type_corp_freq.txt', 'w', encoding='utf-8') as fOut:
+    for paradigm in sorted(paradigmFreqTypeCorp,
+                        key=lambda p: (-paradigmFreqTypeCorp[p], p)):
+        fOut.write(paradigm + '\t' + str(paradigmFreqTypeCorp[paradigm]) + '\n')
+
+with open('paradigm_token_corp_freq.txt', 'w', encoding='utf-8') as fOut:
+    for paradigm in sorted(paradigmFreqToken,
+                        key=lambda p: (-paradigmFreqToken[p], p)):
+        fOut.write(paradigm + '\t' + str(paradigmFreqToken[paradigm]) + '\n')
